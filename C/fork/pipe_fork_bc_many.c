b8c3e9a8eb20acf8299820c957732636e1343810
Gejza <gejza@rorschach>
2025-10-20 12:03:25 +0200

    this

--- C/fork/pipe_fork_bc_many.c
+++ C/fork/pipe_fork_bc_many.c
@@ -11,6 +11,7 @@ int main() {
     int pipefd_w[2];
     int pipefd_r[2];
     int vysledok;
+    int n;
     FILE *fp_w;
     FILE *fp_r;
 
@@ -23,19 +24,31 @@ int main() {
         exit(1);
     }
     if (fork()>0) {
+       // zavrieme nepotrebne konce rur
+       close(pipefd_w[0]);
+       close(pipefd_r[1]);
        fp_w=fdopen(pipefd_w[1],"w");
        fp_r=fdopen(pipefd_r[0],"r");
        // write(pipefd_w[1],"2+1\n",4);
        // read_num=read(pipefd_r[0],buf,sizeof(buf)-1);
        // buf[read_num]=0;
-       fprintf(fp_w,"2+1\n");
+       for (n=1;n<10;n++) {
+            fprintf(fp_w,"2*%d\n",n);
+       }
        fflush(fp_w);
-       fscanf(fp_r,"%d",&vysledok);
-       printf("%d\n",vysledok);
+       fclose(fp_w);
+       close(pipefd_w[1]);
+       while (!feof(fp_r)) {
+            fscanf(fp_r,"%d",&vysledok);
+            printf("%d\n",vysledok);
+       }
     }
     else {
         dup2(pipefd_w[0],0);
         dup2(pipefd_r[1],1);
+        // zavrieme nepotrebne konce rur
+        close(pipefd_w[1]);
+        close(pipefd_r[0]);
         if (execl("/bin/bc",NULL)<0) {
             printf("Chyba pri execl\n");
         }
