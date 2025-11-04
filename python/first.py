6d2cdb7827a8de2cd742ba427456a21eeec0bb35
Gejza <gejza@rorschach>
2025-11-04 15:38:49 +0100

    Prve programy v Python

---
+++ python/first.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+print('Hello, world')
+
+x=1
+y=2
+
+print(x,y)
+if x<y:
+    print('<')
+elif x==y:
+    print('==')
+else:
+    print('>')
+
+print('hotovo')
---
+++ python/print_fibo.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+a,b=1,1
+for count in range(30):
+    print(a)
+    a,b=b,a+b
+
